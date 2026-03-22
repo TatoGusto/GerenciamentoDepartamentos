91553d6c38a4a4230b0dc247f3191228d5c4e4b2
OtavioAugusto <santostatogusto@gmail.com>
2026-03-22 17:44:18 -0300

    service criado

---
+++ GerenciamentoDepartamentos/src/main/java/com/oasw/GerenciamentoDepartamentos/Services/DepartamentoService.java
@@ -0,0 +1,31 @@
+package com.oasw.GerenciamentoDepartamentos.Services;
+
+import com.oasw.GerenciamentoDepartamentos.Models.DepartamentoModel;
+import com.oasw.GerenciamentoDepartamentos.Repositories.DepartamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartamentoService {
+
+    @Autowired
+    DepartamentoRepository departamentoRepository;
+
+    public DepartamentoModel create(DepartamentoModel departamentoModel){
+        return departamentoRepository.save(departamentoModel);
+    }
+
+    public List<DepartamentoModel> getAll(){
+        return departamentoRepository.findAll();
+    }
+
+    public DepartamentoModel getById(Long id){
+        return departamentoRepository.getById(id);
+    }
+
+    public void deleteById(Long id){
+        departamentoRepository.deleteById(id);
+    }
+}
